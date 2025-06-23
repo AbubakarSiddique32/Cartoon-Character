82f6e5388e477ffa7e03a16833a093f0afb75662
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-06-23 19:03:40 +0500

    Cartoon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Cartoon from "./Components/Cartoon";
 
 function App() {
   return (
     <>
       <h1 className=" bg-blue-500">Hello World</h1>
+      <Cartoon />
     </>
   );
 }
--- src/Components/Cartoon.jsx
+++ src/Components/Cartoon.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Cartoon = () => {
-  return <div></div>;
+  return <div>
+   </div>;
 };
 
 export default Cartoon;
