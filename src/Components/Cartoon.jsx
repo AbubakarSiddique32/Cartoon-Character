ecdcf25fbb9443b5f479d3b872b45b97bbbb017a
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-06-23 19:04:09 +0500

    Cartoon

--- src/Components/Cartoon.jsx
+++ src/Components/Cartoon.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
 const Cartoon = () => {
-  return <div>
-   </div>;
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores
+        expedita sunt quod ipsa quia laboriosam provident harum explicabo illum
+        et eligendi, rerum voluptas dignissimos quisquam, unde error quae non?
+        Eum!
+      </p>
+    </div>
+  );
 };
 
 export default Cartoon;
